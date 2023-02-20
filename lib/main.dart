8fac226bd4223ae7c9e6333154391c89d94ecdde
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-02-20 11:40:06 +0530

    added hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
-
+import 'package:hive_flutter/hive_flutter.dart';
 import 'pages/home_page.dart';
 
-void main() {
+void main() async {
+  // init the hive
+  await Hive.initFlutter();
+
+  // open a box
+  var box = await Hive.openBox('mybox');
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
+   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -19,11 +24,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         fontFamily: 'Raleway',
       ),
-      home: MyHomePage(title: "To do App",),
+      home: HomePage(),
     );
   }
 }
-
-
-
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,85 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import '../data/database.dart';
 import '../util/new_task_dialog.dart';
-import '../util/todo_tile.dart';
+import "../util/todo_tile.dart";
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  //todo tasks list
-  List todoList = [
-    ["Make tutorials", false],
-    ["Bake a cake", false],
-  ];
-//text controller
-final _controller = TextEditingController();
+class _HomePageState extends State<HomePage> {
+  // reference the hive box
+  final _myBox = Hive.box('mybox');
+  ToDoDataBase db = ToDoDataBase();
 
-//check box was tapped
+  @override
+  void initState() {
+    // if this is the 1st time ever openin the app, then create default data
+    if (_myBox.get("TODOLIST") == null) {
+      db.createInitialData();
+    } else {
+      // there already exists data
+      db.loadData();
+    }
+
+    super.initState();
+  }
+
+  // text controller
+  final _controller = TextEditingController();
+
+  // checkbox was tapped
   void checkBoxChanged(bool? value, int index) {
     setState(() {
-      todoList[index][1] = !todoList[index][1];
+      db.toDoList[index][1] = !db.toDoList[index][1];
     });
+    db.updateDataBase();
   }
-//save 
-void saveTask(){
-  
-  setState(() {
-    //add task to list, 
-    todoList.add([_controller.text, false]);
-    //clear textbox
-    _controller.clear();
-    
-  } );
-   //close popup after saving task
-  Navigator.of(context).pop();
-}
-//delete task
-void _deleteTask(int index){
-  setState(() {
-    todoList.removeAt(index);
-  });
-}
+
+  // save new task
+  void saveNewTask() {
+    setState(() {
+      db.toDoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+    Navigator.of(context).pop();
+    db.updateDataBase();
+  }
+
+  // create a new task
   void createNewTask() {
     showDialog(
-        context: context,
-        builder: ((context) {
-          return DialogBox(
-            controller: _controller,
-            onCancel: () => Navigator.of(context).pop(),
-            onSave: saveTask,
-          );
-        }));
+      context: context,
+      builder: (context) {
+        return DialogBox(
+          controller: _controller,
+          onSave: saveNewTask,
+          onCancel: () => Navigator.of(context).pop(),
+        );
+      },
+    );
+  }
+
+  // delete task
+  void deleteTask(int index) {
+    setState(() {
+      db.toDoList.removeAt(index);
+    });
+    db.updateDataBase();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 243, 241, 241),
+      backgroundColor: Color.fromARGB(255, 243, 241, 241),
         appBar: AppBar(
           elevation: 0,
           title: Center(child: Text("T O D O  A P P")),
           shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.vertical(bottom: Radius.circular(40))),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: createNewTask,
-          child: Icon(Icons.add),
-        ),
-        body: ListView.builder(
-          itemCount: todoList.length,
-          itemBuilder: (context, index) {
-            return TodoTile(
-              taskCompleted: todoList[index][1],
-              taskName: todoList[index][0],
-              onChanged: (value) => checkBoxChanged(value, index),
-              deleteTask: (context) => _deleteTask(index),
-            );
-          },
-        ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: db.toDoList.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            taskName: db.toDoList[index][0],
+            taskCompleted: db.toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+            deleteTask: (context) => deleteTask(index),
+          );
+        },
+      ),
+    );
   }
 }
+
+//  return Scaffold(
+//         backgroundColor: Color.fromARGB(255, 243, 241, 241),
+//         appBar: AppBar(
+//           elevation: 0,
+//           title: Center(child: Text("T O D O  A P P")),
+//           shape: RoundedRectangleBorder(
+//               borderRadius: BorderRadius.vertical(bottom: Radius.circular(40))),
+//         ),
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: createNewTask,
+//           child: Icon(Icons.add),
+//         ),
+//         body: ListView.builder(
+//           itemCount: db.todoList.length,
+//           itemBuilder: (context, index) {
+//             return TodoTile(
+//               taskCompleted: db.todoList[index][1],
+//               taskName: db.todoList[index][0],
+//               onChanged: (value) => checkBoxChanged(value, index),
+//               deleteTask: (context) => _deleteTask(index),
+//             );
+//           },
+//         ));
